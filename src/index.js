7626c75312d8ae9b837cc735e6158642444b243b
fluxet <fluxet@mail.ru>
2018-12-25 19:20:03 +0300

    Приветствие пользователя v1

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import readlineSync from 'readline-sync';
+
+const greeting = () => {
+	const readlineSync = require('readline-sync');
+
+	// Wait for user's response.
+	const userName = readlineSync.question('May I have your name? ');
+	console.log('Hi ' + userName + '!');
+};
+
+export {greeting};
+ 
