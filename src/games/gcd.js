de8dfd421dc8687dbcfbdd34b739f4ae0ba45e97
fluxet <fluxet@mail.ru>
2018-12-30 14:51:54 +0300

    игра НОД, добавление ф-ции gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,5 +1,21 @@
 import { getRandomNumber, maxNumber, driver } from '..';
 
+const gcd = (arg1, arg2) => {
+  const minArg = (arg1 < arg2) ? arg1 : arg2;
+
+  const iter = (counter) => {
+    if (counter <= 1) {
+      return 1;
+    }
+    const isDivisor = ((arg1 % counter === 0) && (arg2 % counter === 0));
+    if (isDivisor) {
+      return counter;
+    }
+    return iter(counter - 1);
+  };
+  return iter(minArg);
+};
+
 const showTaskGcd = () => {
   console.log('Find the greatest common divisor of given numbers.');
   console.log(' ');
@@ -11,21 +27,7 @@ const makeQuestionGcd = () => ({
 });
 
 const showQuestionGcd = question => `Question: ${question.arg1} ${question.arg2}`;
-const makeSolutionGcd = (question) => {
-  const minArg = (question.arg1 < question.arg2) ? question.arg1 : question.arg2;
-
-  const iter = (counter) => {
-    if (counter <= 1) {
-      return 1;
-    }
-    const isDivisor = ((question.arg1 % counter === 0) && (question.arg2 % counter === 0));
-    if (isDivisor) {
-      return counter;
-    }
-    return iter(counter - 1);
-  };
-  return iter(minArg);
-};
+const makeSolutionGcd = question => gcd(question.arg1, question.arg2);
 
 const runGameGcd = () => {
   driver(showTaskGcd, makeQuestionGcd, showQuestionGcd, makeSolutionGcd);
